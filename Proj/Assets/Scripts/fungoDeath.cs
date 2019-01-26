5cc80b870597412353d44b3c02e06c7d72d56a85
snarmph <43290148+snarmph@users.noreply.github.com>
2019-01-26 17:41:07 +0100

    aggiunto fungo collider

---
+++ Proj/Assets/Scripts/fungoDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fungoDeath : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "MainCharacter")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
