1e495d2245e96bdb1b75b37ed4d19a2db212f08d
snarmph <43290148+snarmph@users.noreply.github.com>
2019-01-26 15:48:02 +0100

    aggiunto fungo

---
+++ Proj/Assets/Scripts/fungo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fungo : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.name == "MainCharacter")
+        {
+            Vector3 pos1 = this.transform.position;
+            Vector3 pos2 = MainCharacter.shared.transform.position;
+            float direction = MainCharacter.GetDirection(pos1, pos2);
+            MainCharacter.shared.direction = direction;
+        }
+    }
+}
