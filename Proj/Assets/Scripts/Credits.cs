4914cc4f5b77ffc621c39f342f03d3c47f26bd90
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2019-01-27 15:17:46 +0100

    Aggiunti credits

--- Proj/Assets/Scripts/Credits.cs
+++ Proj/Assets/Scripts/Credits.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CreditsButton : MonoBehaviour
+public class Credits : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,6 @@ public class CreditsButton : MonoBehaviour
 
     private void OnMouseDown()
     {
-        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
     }
 }
